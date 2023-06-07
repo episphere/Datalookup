50108427215571fd7bfe0cb5f71bf7d28e41bf1e
Jonas Almeida <jalmeida@mathbiol.org>
2023-06-07 17:03:45 -0400

    intruding viewport in root

---
+++ jonas/export.js
@@ -0,0 +1,21 @@
+// experimenting with communicating risk visually
+
+// default tables
+
+let tbls = [
+    "Post-Colpo_v5.xlsx",
+    "Post-Treatment_v2.xlsx",
+    "Risk Following Colposcpy.xlsx",
+    "Screening v5.xlsx",
+    "Surveillance_v6.xlsx"
+]
+
+let UI = function(div=document.createElement('div')){
+    
+}
+
+
+export {
+    tbls,
+    UI
+}
--- jonas/index.js
+++ jonas/index.js
@@ -1,2 +1,5 @@
-console.log(`${location.href+'index.js'} loaded
-${Date()}`)
+console.log(`${location.href+'index.js'} \nloaded ${Date()}`);
+
+(async function(){
+    rv = await import('./export.js')
+})();

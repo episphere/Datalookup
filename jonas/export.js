802f04ffb33cff024cecd41ede831fd6157feae1
Jonas Almeida <jalmeida@mathbiol.org>
2023-06-13 11:17:49 -0400

    ini tabulation div

--- jonas/export.js
+++ jonas/export.js
@@ -2,16 +2,43 @@
 
 // default tables
 
-let tbls = [
-    "Post-Colpo_v5.xlsx",
-    "Post-Treatment_v2.xlsx",
-    "Risk Following Colposcpy.xlsx",
-    "Screening v5.xlsx",
-    "Surveillance_v6.xlsx"
+const tbls = [
+    "General Table for Post-Colpo_v5.xlsx",
+    "General Table for Post-Treatment_v2.xlsx",
+    "General Table for Risk Following Colposcpy.xlsx",
+    "General Table for Screening v5.xlsx",
+    "General Table for Surveillance_v6.xlsx"
 ]
 
-let UI = function(div=document.createElement('div')){
-    
+function UI(div=document.getElementById('vizDiv')){
+    if(!div){ // create div if it doesn't exist
+        div=document.createElement('div')
+        document.body.appendChild(div) // and add it to the DOM
+    }
+    if(typeof(div)=='string'){ // you can also call it by the id
+        div=document.getElementById(div)
+    }
+    div.innerHTML='select data table <select id="selTable"></select>'
+    let selTable = div.querySelector('#selTable')
+    tbls.forEach(x=>{
+        console.log(x)
+        let opt = document.createElement('option')
+        opt.textContent=opt.value=x
+        selTable.appendChild(opt)
+    })
+    let tabDiv=document.createElement('div') // tabulation div
+    div.appendChild(tabDiv)
+    tabDiv.id='tabDiv' // in case one needs to fish it later
+    selTable.onchange=function(div=tabDiv){
+        tabulate(tabDiv,selTable.value)
+    }
+    tabulate(tabDiv,selTable.value) // initial default tabulation
+}
+
+async function tabulate(div,tb){
+    div.innerHTML=tb
+    let dt = await fetch(`../data/${encodeURIComponent(tb)}`)
+    console.log(dt)
 }
 
 
--- jonas/index.js
+++ jonas/index.js
@@ -2,4 +2,6 @@ console.log(`${location.href+'index.js'} \nloaded ${Date()}`);
 
 (async function(){
     rv = await import('./export.js')
+    rv.UI() // open UI in a blank div added to the document body
+    
 })();

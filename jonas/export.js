98565ca91a31d475e353f6a290777a24a5fd2c96
Jonas Almeida <jalmeida@mathbiol.org>
2023-06-13 12:16:16 -0400

    loading tables

--- jonas/export.js
+++ jonas/export.js
@@ -1,7 +1,8 @@
 // experimenting with communicating risk visually
 
-// default tables
+import * as xlsx from "https://cdn.sheetjs.com/xlsx-0.19.2/package/xlsx.mjs"
 
+// default tables
 const tbls = [
     "General Table for Post-Colpo_v5.xlsx",
     "General Table for Post-Treatment_v2.xlsx",
@@ -18,7 +19,7 @@ function UI(div=document.getElementById('vizDiv')){
     if(typeof(div)=='string'){ // you can also call it by the id
         div=document.getElementById(div)
     }
-    div.innerHTML='select data table <select id="selTable"></select>'
+    div.innerHTML='select data table <select id="selTable"></select> <span id="source">...</span>'
     let selTable = div.querySelector('#selTable')
     tbls.forEach(x=>{
         console.log(x)
@@ -29,18 +30,21 @@ function UI(div=document.getElementById('vizDiv')){
     let tabDiv=document.createElement('div') // tabulation div
     div.appendChild(tabDiv)
     tabDiv.id='tabDiv' // in case one needs to fish it later
-    selTable.onchange=function(div=tabDiv){
+    let srcSpan = div.querySelector('#source')
+    selTable.onchange=function(evt){
+        srcSpan.innerHTML='...'
         tabulate(tabDiv,selTable.value)
     }
     tabulate(tabDiv,selTable.value) // initial default tabulation
 }
 
-async function tabulate(div,tb){
-    div.innerHTML=tb
+async function tabulate(tabDiv,tb){
+    let url=`${location.origin+location.pathname.replace(/[^\/]+\/$/,'')}data/${encodeURIComponent(tb)}`
     let dt = await fetch(`../data/${encodeURIComponent(tb)}`)
     console.log(dt)
+    let srcSpan = tabDiv.parentElement.querySelector('#source')
+    srcSpan.innerHTML=`<a href="${url}" target="_blank">source</a>`
 }
-
 
 export {
     tbls,

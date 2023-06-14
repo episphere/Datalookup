f65580a85986d1b95f15366ba7741b5ec09018f4
Jonas Almeida <jalmeida@mathbiol.org>
2023-06-13 21:37:52 -0400

    getting used to mjs ...

--- jonas/index.js
+++ jonas/index.js
@@ -1,6 +1,6 @@
 console.log(`${location.href+'index.js'} \nloaded ${Date()}`);
 
 (async function(){
-    rv = await import('./export.js')
+    rv = await import('./riskVizMobile.mjs')
     rv.UI() // open UI in a blank div added to the document body
 })();
